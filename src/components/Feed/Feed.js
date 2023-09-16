9c4aeb2f9bbabeffedb26a58e543b0a9d5364fcd
Uzezi Henry Ovraiti <uovraiti@yahoo.com>
2023-09-16 16:30:37 +0100

    complete GUI of info Page

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -88,6 +88,134 @@ const Feed = () => {
           </span>
         </div>
       </div>
+      <div className="grid grid-cols-12 py-5">
+        <div className="col-span-9">
+          <div className="p-5">
+            <div className="flex flex-col max-w-[774px]  ">
+              <div className="flex">
+                <span className="">Top Gun Maverick </span>
+                <span className="">. 2022</span>
+                <span className="">PG-13 . 2h 10m</span>
+                <span className="">Genre Genre</span>
+              </div>
+              <div className="mt-5">
+                After thirty years, Maverick is still pushing the envelope as a
+                top naval aviator, but must confront ghosts of his past when he
+                leads TOP GUN's elite graduates on a mission that demands the
+                ultimate sacrifice from those chosen to fly it.
+              </div>
+            </div>
+            <div className="flex flex-col justify-between gap-7">
+              <div className="">
+                <span className="font-normal text-[20px] text-[#333333]">
+                  Director :{" "}
+                </span>
+                <span className="text-[#BE123C] font-sans font-bold text-[20px]">
+                  Joseph Kosinski
+                </span>
+              </div>
+              <div className="">
+                <span className="font-normal text-[20px] text-[#333333]">
+                  Writers :{" "}
+                </span>
+                <span className="text-[#BE123C] font-sans font-bold text-[20px]">
+                  Jim Cash, Jack Epps Jr, Peter Craig
+                </span>
+              </div>
+              <div className="">
+                <span className="font-normal text-[20px] text-[#333333]">
+                  Stars :{" "}
+                </span>
+                <span className="text-[#BE123C] font-sans font-bold text-[20px]">
+                  Tom Cruise, Jennifer Connelly, Miles TellerJoseph Kosinski
+                </span>
+              </div>
+              <div className="flex w-full">
+                <button
+                  className="bg-[#BE123C] rounded-[10px] h-14 w-[253px] text-white"
+                  type="button"
+                >
+                  Top rated movie #65
+                </button>
+                <div className="flex border-black border-y-2 border-r-2 rounded-[10px] h-14 w-full text-white justify-between ">
+                  <span className="text-black items-center h-full flex ms-8">
+                    Awards 9 nominations
+                  </span>
+                  <Image
+                    src={"/ExpandArrow.png"}
+                    width={54}
+                    height={54}
+                    alt={"play"}
+                    className="p-3"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-span-3 px-4">
+          <div className="flex justify-end w-full items-center">
+            <Image
+              src={"/Star.png"}
+              width={54}
+              height={54}
+              alt={"play"}
+              className="p-4"
+            />
+            <div className="text-[20px] text-[#666]">
+              {" "}
+              <span className="text-[#E8E8E8] text-[25px]">8.5 </span>| 350k
+            </div>
+          </div>
+          <button
+            className="bg-[#BE123C] flex items-center justify-center rounded-[10px] h-14 w-full text-white"
+            type="button"
+          >
+            <Image
+              src={"/TwoTickets.png"}
+              width={54}
+              height={54}
+              alt={"play"}
+              className="p-3"
+            />
+            See Showtimes
+          </button>
+          <button
+            className="bg-[#BE123C1A] border-[#BE123C] border-2 text-black my-3 flex items-center justify-center rounded-[10px] h-14 w-full "
+            type="button"
+          >
+            <Image
+              src={"/List.png"}
+              width={54}
+              height={54}
+              alt={"play"}
+              className="p-3"
+            />
+            See Showtimes
+          </button>
+          <div className="">
+            <Image
+              src={"/MorePics.png"}
+              width={760}
+              height={428}
+              alt={"play"}
+              className=" w-full h-[229px]"
+            />
+            <div className="relative bottom-8  h-[30px] w-full flex items-center justify-center bg-[#12121280] rounded-[10px] ">
+              <Image
+                src={"/List.png"}
+                width={54}
+                height={54}
+                alt={"play"}
+                className="p-4"
+              />
+              <span className="text-[12px] text-white">
+                The Best Movies and Shows in September
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
